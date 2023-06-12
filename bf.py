977975a819db4d80243b2de0295deb22b8837f58
Dmitriy Polyakov <ddpolyakov@gmail.com>
2023-06-12 22:23:58 +0200

    add safe div

--- bf.py
+++ bf.py
@@ -1,5 +1,7 @@
 #!/bin/python3
+import re
 import subprocess, json, argparse
+import datetime
 def init_pg(cluster=None):
   if not cluster:
       cluster = "ceph"
@@ -14,7 +16,28 @@ def init_from_file():
     # print(pg_data)
     return pg_data['pg_map']
 
-
+def pgs_scrub(cluster,use_file):
+    if use_file:
+        stats = init_from_file()['pg_stats']
+    else:
+        stats = init_pg(cluster=cluster)['pg_stats']
+    scrub_list = []
+    deep_scrub_list = []
+    for pg in stats:
+        pgid = pg['pgid']
+        # print(pg.keys())
+        sdate = pg["last_scrub_stamp"]
+        dsdate = pg["last_scrub_stamp"]
+        deep_scrub_list.append([pgid, datetime.datetime.strptime(dsdate, "%Y-%m-%d %H:%M:%S.%f"),pg["state"]])
+        scrub_list.append([pgid, datetime.datetime.strptime(sdate, "%Y-%m-%d %H:%M:%S.%f"),pg["state"]])
+    print("deepscrub:")
+    for p in sorted(deep_scrub_list, key = lambda i: i[1]):
+        print("%s\t%s\t%s" % (p[0],p[1],p[2]))
+    print("scrub: ")
+    for p in sorted(scrub_list, key = lambda i: i[1]):
+        print("%s\t%s\t%s" % (p[0],p[1],p[2]))
+    
+    return()
 def pgs_backfill(action,osd,cluster,use_file):
     global_progress_list = []
     if osd: osd = int(osd)
@@ -70,7 +93,7 @@ def pg_print(pg=None, action=None):
     num_objects = pg['stat_sum']['num_objects']
     num_objects_copies = pg['stat_sum']['num_object_copies']
     num_faulty_objects = max([pg['stat_sum']['num_objects_degraded'],pg['stat_sum']['num_objects_misplaced']])
-    object_perc = 1 - float(num_faulty_objects) / float(num_objects_copies)
+    object_perc = 1 - (float(num_faulty_objects) / float(num_objects_copies) if num_objects_copies > 0 else 0)
     print("pg %s %s from osd %s to osd %s size of %.2f, progress: %.2f" % (pgid, state,where_from, where_to, num_gb, object_perc))
     # return(object_perc)
 
@@ -82,10 +105,19 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--state", help = "state osd")
     parser.add_argument("-c", "--cluster", help = "cluster name")  
     parser.add_argument("-f", "--file", help="use file instead of output (ONLY FOR LOCAL DEBUG RUN)", action='store_true')  
+    parser.add_argument("-a", "--action", help="scrub or backfill", default="backfill")
     args = parser.parse_args()
     osd = args.osd
     action = args.state
     cluster = args.cluster
     use_file = args.file
-    print(action,type(action),use_file,type(use_file))
-    pgs_backfill(action,osd,cluster,use_file)
+    action = args.action
+    print(action)
+    if (not action or action == "backfill"):
+        print(action,type(action),use_file,type(use_file))
+        pgs_backfill(action,osd,cluster,use_file)
+    elif action == "scrub":
+        pgs_scrub(cluster,use_file)
+    else: 
+        parser.print_help()
+    
